8029a9071864717072393cddfa6d246b8a6407bc
dv-udit <139757768+dv-udit@users.noreply.github.com>
2024-03-02 13:51:35 +0530

    Update prompt

--- code/prompt_template.py
+++ code/prompt_template.py
@@ -1,15 +1,13 @@
 # prompt_template.py
 PROMPT_TEMPLATE = """
-You're an esteemed college professor tasked with crafting {{ type }} questions based on the {{ content }} provided. Your objective is to design questions that evaluate students' understanding and application of the content, using the {{learning_objective}} and {{Keywords}} provided. Each question should focus on a single concept and written in the singular tense. Aim for a balanced level of challenge and solvability, avoiding questions that are overly simplistic or excessively difficult. Design questions based on Bloom's Taxonomy level {{ level }} to assess different aspects of student learning.  Enhance student engagement by providing concrete examples or scenarios within the questions, facilitating connections to real-world contexts. Ensure meticulous proofreading and editing for clarity, conciseness, and grammatical accuracy. Aim to generate a minimum of 10 questions, adjusting the count based on the length and complexity of the content, while maintaining high-quality standards.
+As an esteemed college professor tasked with crafting {{ type }} questions based on the {{ content }} provided, your objective is to design questions that align properly with the given {{learning_objective}} and {{Keywords}}, evaluating students' understanding and application of the content. Each question should focus on a single concept and be written in the singular tense, with correct answers not exposed within the questions generated. Ensure that correct answers are the best possible answers, while distractors should be plausible alternatives of similar complexity, detail, and length. Aim for a balanced level of challenge and solvability, avoiding questions that are overly simplistic or excessively difficult. Design questions based on Bloom's Taxonomy level {{ level }} to assess different aspects of student learning. Enhance student engagement by providing concrete examples or scenarios within the questions, facilitating connections to real-world contexts. Ensure meticulous proofreading and editing for clarity, conciseness, and grammatical accuracy. Aim to generate a minimum of 10 questions, adjusting the count based on the length and complexity of the content, while maintaining high-quality standards.
 
 For MCQS type questions, provide four options for each question, ensuring a mix of one-word choices and complete sentences or phrases, while shuffling the correct answer among relevant distractors to foster critical thinking. Refrain from utilizing specific option types like "All of the above," "None of the above," or "Both A and B," while ensuring options remain discernible yet require critical thinking to distinguish the correct choices from distractors. For Fill in the Blanks type questions, utilize a blank space with five underscores as placeholders for options.
 
 Please respond to each question in the following format:
 
 {% if type == "MCQs" %}
- 
     Question with its respective options:
-
     Option A
     Option B
     Option C
@@ -18,14 +16,10 @@ Please respond to each question in the following format:
     Blooms Taxonomy Level:
 
 {% elif type == "Fill in the Blanks" %}
-
     Correct answer: 
     Blooms Taxonomy Level:
 
-
-    
 {% elif type == "True/False" %}
-
     Options A: True/False
     Options B: True/False
     Correct answer:
