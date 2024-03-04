38405478c53d34038770cf01df4c02362ebdbc52
Amit Mujawar <1662670+amit10may@users.noreply.github.com>
2024-03-04 20:40:22 +0530

    updated prompts and minor labels
    added prompt for FIB placeholder, ensure blooms level sticks to one given.

--- demo_version2/prompt_template.py
+++ demo_version2/prompt_template.py
@@ -1,14 +1,16 @@
 # prompt_template.py
 PROMPT_TEMPLATE = """
-As an esteemed college professor tasked with crafting {{ type }} questions based on the {{ content }} provided, your objective is to design questions that align properly with the given {{learning_objective}} and {{Keywords}}, evaluating students' understanding and application of the content. Each question should focus on a single concept and be written in the singular tense, with correct answers not exposed within the questions generated. Ensure that correct answers are the best possible answers, while distractors should be plausible alternatives of similar complexity, detail, and length. Aim for a balanced level of challenge and solvability, avoiding questions that are overly simplistic or excessively difficult. Design questions based on Bloom's Taxonomy level {{ level }} to assess different aspects of student learning. Enhance student engagement by providing concrete examples or scenarios within the questions, facilitating connections to real-world contexts. Ensure meticulous proofreading and editing for clarity, conciseness, and grammatical accuracy. Aim to generate a minimum of 10 questions, adjusting the count based on the length and complexity of the content, while maintaining high-quality standards.
+As an esteemed college professor tasked with crafting {{ type }} questions based on the {{ content }} provided, your objective is to design questions that align properly with the given {{learning_objective}} and {{Keywords}}, evaluating students' understanding and application of the content. Each question should focus on a single concept and be written in the singular tense, with correct answers not exposed within the questions generated. Ensure that correct answers are the best possible answers, while distractors should be plausible alternatives of similar complexity, detail, and length. Aim for a balanced level of challenge and solvability, avoiding questions that are overly simplistic or excessively difficult. Design questions based on Bloom's Taxonomy level {{ level }} to assess different aspects of student learning. All questions should be of the same taxonomy level - {{ level }}. Enhance student engagement by providing concrete examples or scenarios within the questions, facilitating connections to real-world contexts. Ensure meticulous proofreading and editing for clarity, conciseness, and grammatical accuracy. Aim to generate a minimum of 10 questions, adjusting the count based on the length and complexity of the content, while maintaining high-quality standards.
+{% if type == "Fill in the blanks" %}
+For Fill in the blanks type questions, use _____ as placeholder in the stem as a blank space.
+{% endif %}
 
-
-Please respond to each question in the following {{format}}:
+Please format output in the following format - {{format}}:
 {% if format == "json_format" %}
-    {% if type == "Multiple Type Questions" %}
+    {% if type == "Multiple choice" %}
     [{
       "question_id": "<number>",
-      "type": "Multiple Choice Question",
+      "type": "Multiple choice",
       "stem": "<main body of the question>",
       "options": {
         "a": "Option1",
@@ -16,16 +18,15 @@ Please respond to each question in the following {{format}}:
         "c": "Option3",
         "d": "Option4"
       },
-      "rationale": "if Correct why?, if incorrect why?",
+      "rationale": "if correct why?, if incorrect why?",
       "correct_option": "<correct_option>",
       "blooms_taxonomy_level": "<level of blooms taxonomy>",
-      "difficulty_level": "<High/Medium/Low>",
-      "student_level": "<Elementary/High School/College/University>"
+      "difficulty_level": "<High/Medium/Low>"
     }]
-    {% elif type == "Fill In The Blanks" %}
+    {% elif type == "Fill in the blanks" %}
     [{
       "question_id": "<number>",
-      "type": "Fill in the Blanks",
+      "type": "Fill in the blanks",
       "stem": "<main body of the question>",
       "options": {
         "a": "_____",
@@ -33,30 +34,28 @@ Please respond to each question in the following {{format}}:
         "c": "_____",
         "d": "_____"
       },
-      "rationale": "if Correct why?, if incorrect why?",
+      "rationale": "if correct why?, if incorrect why?",
       "correct_option": "<correct_option>",
       "blooms_taxonomy_level": "<level of blooms taxonomy>",
-      "difficulty_level": "<High/Medium/Low>",
-      "student_level": "<Elementary/High School/College/University>"
+      "difficulty_level": "<High/Medium/Low>"
     }]
-    {% elif type == "True False" %}
+    {% elif type == "True or false" %}
     [{
       "question_id": "<number>",
-      "type": "True/False",
+      "type": "True or false",
       "stem": "<main body of the question>",
       "options": {
         "a": "True",
         "b": "False"
       },
-      "rationale": "if Correct why?, if incorrect why?",
+      "rationale": "if correct why?, if incorrect why?",
       "correct_option": "<correct_option>",
       "blooms_taxonomy_level": "<level of blooms taxonomy>",
-      "difficulty_level": "<High/Medium/Low>",
-      "student_level": "<Elementary/High School/College/University>"
+      "difficulty_level": "<High/Medium/Low>"
     }]
-    {% endif %}
+{% endif %}
 {% elif format == "plain_text" %}
-    {% if type == "Multiple Type Questions" %}
+    {% if type == "Multiple choice" %}
         Question with its respective options:
         Option A
         Option B
@@ -65,13 +64,13 @@ Please respond to each question in the following {{format}}:
         Correct answer:
         Blooms Taxonomy Level:
 
-    {% elif type == "Fill In The Blanks" %}
+    {% elif type == "Fill in the blanks" %}
         Correct answer: 
         Blooms Taxonomy Level:
 
-    {% elif type == "True False" %}
-        Options A: True/False
-        Options B: True/False
+    {% elif type == "True or false" %}
+        Options A: True
+        Options B: False
         Correct answer:
         Blooms Taxonomy Level:
 
